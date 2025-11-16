7dcf4deb623ff197ad2f98a1d535eb2102daddd5
Laísa Albuquerque <laisaa275@gmail.com>
2025-11-16 17:33:56 -0300

    feat(products): criar listagem de produtos com filtros e ordenação

--- src/app/features/products-list/products-list.component.ts
+++ src/app/features/products-list/products-list.component.ts
@@ -6,43 +6,97 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
   products: Product[] = [];
   filtered: Product[] = [];
-
-  // TODO: implementar searchTerm e selectedCategory para o filtro
   searchTerm = '';
   selectedCategory = '';
+  sortColumn: string = '';
+  sortDirection: 'asc' | 'desc' = 'asc';
 
-  // TODO: usar essa lista de categorias no select de filtro
-  get categories(): string[] {
-    return [...new Set(this.products.map(p => p.category))];
-  }
+  // tooltip
+  tooltipX = 0;
+  tooltipY = 0;
+  showTooltip = false;
 
-  constructor(
-    private productService: ProductService,
-    private router: Router
-  ) {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
-    // Carrega a lista inicial de produtos
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
-      this.filtered = data;
+      this.filtered = [...data];
     });
   }
 
+  get categories(): string[] {
+    return [...new Set(this.products.map((p) => p.category))];
+  }
+
   applyFilter(): void {
-    // TODO:
+    this.filtered = this.products.filter((product) => {
+      const nameMatch = product.name
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase());
+      const categoryMatch = this.selectedCategory
+        ? product.category === this.selectedCategory
+        : true;
+      return nameMatch && categoryMatch;
+    });
+    if (this.sortColumn) {
+      this.sortBy(this.sortColumn as keyof Product, true);
+    }
   }
 
   clearFilter(): void {
-    // TODO:
+    this.searchTerm = '';
+    this.selectedCategory = '';
+    this.filtered = [...this.products];
+    if (this.sortColumn) {
+      this.sortBy(this.sortColumn as keyof Product, true);
+    }
+  }
+
+  sortBy(column: keyof Product, keepDirection = false): void {
+    if (!keepDirection) {
+      if (this.sortColumn === column) {
+        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      } else {
+        this.sortDirection = 'asc';
+      }
+    }
+    this.sortColumn = column;
+
+    this.filtered.sort((a, b) => {
+      let valueA = a[column];
+      let valueB = b[column];
+
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return this.sortDirection === 'asc' ? valueA - valueB : valueB - valueA;
+      }
+
+      return this.sortDirection === 'asc'
+        ? String(valueA).localeCompare(String(valueB))
+        : String(valueB).localeCompare(String(valueA));
+    });
   }
 
   goToDetail(product: Product): void {
-    // TODO:
+    this.router.navigate(['/products', product.id]);
+  }
+
+  // Tooltip
+  onMouseMove(event: MouseEvent) {
+    this.tooltipX = event.pageX + 15;
+    this.tooltipY = event.pageY + 15;
+  }
+
+  onMouseEnterRow() {
+    this.showTooltip = true;
+  }
+
+  onMouseLeaveRow() {
+    this.showTooltip = false;
   }
 }
