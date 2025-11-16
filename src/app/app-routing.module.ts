f70ce5f27d4a9a05dc99c6fc686d0a1b28275973
Laísa Albuquerque <laisaa275@gmail.com>
2025-11-16 17:47:33 -0300

    fix: corrige as rotas e declrações de modulos da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsListComponent } from './features/products-list/products-list.component';
 import { ProductDetailComponent } from './features/product-detail/product-detail.component';
+import { ProductNewComponent } from './features/product-new/product-new.component';
+import { ProductEditComponent } from './features/product-edit/product-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'products', component: ProductsListComponent },
+  { path: 'products/new', component: ProductNewComponent },
   { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'products/edit/:id', component: ProductEditComponent },
   { path: '**', redirectTo: 'products' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductsListComponent } from './features/products-list/products-list.component';
 import { ProductDetailComponent } from './features/product-detail/product-detail.component';
+import { ProductNewComponent } from './features/product-new/product-new.component';
+import { ProductEditComponent } from './features/product-edit/product-edit.component';
+import { ConfirmationModalComponent } from './shared/confirmation-modal/confirmation-modal.component';
+import { HeaderComponent } from './shared/header/header.component';
+
+import { LucideAngularModule, RefreshCcw, Plus, Edit, Package, ArrowUpDown, ChevronUp, ChevronDown, Save, X, Pencil, Trash2, ArrowLeft  } from 'lucide-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductNewComponent,
+    ProductEditComponent,
+    ConfirmationModalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    LucideAngularModule.pick({ RefreshCcw, Plus, Edit, Package, ArrowUpDown, ChevronUp, ChevronDown, Save, X, Pencil, Trash2, ArrowLeft })
   ],
   bootstrap: [AppComponent]
 })
