2ac20baf3c6e8002e2612ce6a180430c522468b6
Laísa Albuquerque <laisaa275@gmail.com>
2025-11-16 17:46:43 -0300

    feat(products): estilização visual e funcionalidades da página detail-list

--- src/app/features/product-detail/product-detail.component.ts
+++ src/app/features/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../../core/models/product.model';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -10,13 +11,30 @@ import { Product } from '../../core/models/product.model';
 export class ProductDetailComponent implements OnInit {
   product?: Product;
   isLoading = true;
+  showDeleteModal = false;
 
   constructor(
     private router: Router,
-  ) {}
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
-    // TODO:
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.productService.getProductById(id).subscribe((data) => {
+      this.product = data;
+      this.isLoading = false;
+    });
+  }
+
+  deleteProduct(): void {
+    if (!this.product) return;
+
+    this.productService.deleteProduct(this.product.id);
+
+    this.showDeleteModal = false;
+    this.router.navigate(['/products']);
   }
 
   goBack(): void {
